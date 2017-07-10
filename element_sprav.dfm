d61cd63ab8b60f2c5975b4e70cb8618322a0d8f2
senanta <senanta@inbox.ru>
2017-07-10 18:43:38 +0300

    Test from master note Signed-off-by: senanta <senanta@inbox.ru>

--- element_sprav.dfm
+++ element_sprav.dfm
@@ -2,4 +2,18 @@ inherited FormElementSprav: TFormElementSprav
   Caption = 'FormElementSprav'
   PixelsPerInch = 96
   TextHeight = 13
+  inherited PanelMiddle: TPanel
+    object DBVertGridEh1: TDBVertGridEh
+      Left = 8
+      Top = 6
+      Width = 300
+      Height = 171
+      AllowedSelections = []
+      RowCategories.CategoryProps = <>
+      PrintService.ColorSchema = pcsFullColorEh
+      DrawGraphicData = True
+      DrawMemoText = True
+      TabOrder = 0
+    end
+  end
 end
--- element_sprav.pas
+++ element_sprav.pas
@@ -5,13 +5,14 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, element, System.Actions, Vcl.ActnList,
-  Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.StdCtrls, Vcl.ExtCtrls, EhLibVCL, GridsEh, DBAxisGridsEh, DBVertGridsEh;
 
 ///<summary>
 /// Дочерняя форма элемента справочников - наследуем от TFormElement
 ///</summary>
 type
   TFormElementSprav = class(TFormElement)
+    DBVertGridEh1: TDBVertGridEh;
   private
     { Private declarations }
   public
